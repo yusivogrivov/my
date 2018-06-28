9b66aed95cf6ff9d460e436e4e5a50a901bad12e
Yuriy Sivogrivov <yusivogrivov@gmail.com>
2018-06-28 15:02:35 +0300

    change html, add clients, test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,35 @@ import client from './client';
 import './App.css';
 
 
+function Search () {
+    return (
+           <div class="ui search">
+          <div class="ui icon input">
+            <input class="prompt" type="text" placeholder="Search countries..."></input>
+            <i class="search icon"></i>
+          </div>
+          <div class="results"></div>
+        </div>
+        
+    )}
+
 
 function App() {
     return (
-        <div>
-            <Article myclient={client[0]} />
+        <div> <Search /> 
+           <Article myclient={client[0]} /> 
+           <Article myclient={client[1]} /> 
+           <Article myclient={client[2]} /> 
+           <Article myclient={client[3]} /> 
+           <Article myclient={client[4]} /> 
+           <Article myclient={client[5]} /> 
         </div>
-    )
-}
+      
+     
+            )}
+  
+    
 
-export default App;
+
+
+  export default App;
--- my-app/src/Article.js
+++ my-app/src/Article.js
@@ -1,11 +1,16 @@
+
+import './index.css';
 import React from 'react'
 
 function Clients(props) {
     const {myclient} = props
         return (
             
-            <div>
-                <h1>{myclient.general.firstName}</h1>
+            
+            <div class = "boxclient">
+                <img class="simg" src='https://s3.amazonaws.com/uifaces/faces/twitter/kevinoh/128.jpg' />
+                <p class = "clientname">{myclient.general.firstName}  {myclient.general.lastName}</p>
+                <p class = "clienttitle">{myclient.job.title}</p>  
             </div>
         )
 }
